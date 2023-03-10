565259fcbefe986b57a6d867f45235795f587cbe
LuckDev <lukedru@yahoo.com>
2023-03-10 11:03:55 -0800

    Initial Commit

---
+++ CycleLock/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CycleLock
+//
+//  Created by Luke Drushell on 3/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CycleLock/CycleLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  CycleLockApp.swift
+//  CycleLock
+//
+//  Created by Luke Drushell on 3/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CycleLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
